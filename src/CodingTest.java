8eac36ef64e6ae1c29853deec183e49f57b66816
hyunholee <happydog4927@gmail.com>
2021-03-01 18:08:09 +0900

    Initial commit

---
+++ src/CodingTest.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CodingTest {
+
+    public static String solution(int[] numbers) {
+
+        String answer = new String();
+        /** 1 **/
+        String str_numbers[] = new String[numbers.length];
+
+        /** 2 **/
+        for(int i=0; i<str_numbers.length; i++) {
+            str_numbers[i] = String.valueOf(numbers[i]);
+        }
+
+        /** 3 **/
+        Arrays.sort(str_numbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        /** 4 **/
+        if(str_numbers[0].startsWith("0")) {
+            answer += "0";
+        } else {
+            for(int j=0; j<str_numbers.length; j++) {
+                answer += str_numbers[j];
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] odds = {1,23,51,7,9};
+        System.out.println(solution(odds));
+    }
+}
+
+
+
+
+
+
